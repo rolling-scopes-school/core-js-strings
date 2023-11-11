919e8887abe10d65cddf601fc2cc51b420be8062
Natein <natein@gmail.com>
2023-11-11 08:08:24 +0200

    fix: example in function sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -174,7 +174,7 @@ function removeLastOccurrences(/* str, value */) {
  *
  * @example
  *   sumOfCodes('My String') => 861 (77 + 121 + 32 + 83 + 116 + 114 + 105 + 110 + 103 = 861)
- *   sumOfCodes('12345') => 495 (9 + 50 + 51 + 52 + 53 = 495)
+ *   sumOfCodes('12345') => 255 (49 + 50 + 51 + 52 + 53 = 255)
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
