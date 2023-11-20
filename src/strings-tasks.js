104bb1f789ffba609590eeee67e3a7bdfaa2618b
NatalyKisialiova <nataly.kisialiova@gmail.com>
2023-11-21 00:14:58 +0200

    feat: solve task (function reverseString())

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -284,10 +284,9 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  // const arr = str.split('').reverse().join('');
-  // return arr;
-  throw new Error('Not implemented');
+function reverseString(str) {
+  const arr = str.split('').reverse().join('');
+  return arr;
 }
 
 /**
