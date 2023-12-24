92192e56d34ef8fba70585c0bb58909ae118a52a
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-12-24 19:47:40 +0300

    feat: solve extractEmails, unbracketTag

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -412,8 +412,8 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 /**
@@ -431,8 +431,8 @@ function unbracketTag(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
