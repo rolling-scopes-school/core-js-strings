ad57f7ac4efc1b3db78c10f47c247405565b7317
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-12-24 17:34:14 +0300

    feat: solve endsWith, startsWith

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -201,8 +201,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -216,8 +216,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
