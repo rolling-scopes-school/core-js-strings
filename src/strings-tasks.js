9fdbfc95bd7a37c4013fe035a1b243291bb6ecd5
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-12-24 17:23:37 +0300

    feat: solve removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -163,8 +163,11 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const index = str.lastIndexOf(value);
+  return index > -1
+    ? str.slice(0, index) + str.slice(index + value.length)
+    : str;
 }
 
 /**
