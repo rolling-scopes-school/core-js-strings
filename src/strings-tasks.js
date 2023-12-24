77ad14e19ec9f6d6a9498ac2e6063b59a965fdc8
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-12-24 19:15:17 +0300

    feat: solve findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -331,8 +331,8 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence.split(' ').sort((a, b) => b.length - a.length)[0];
 }
 
 /**
