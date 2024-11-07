fd171d1caf7746dd05ca3521d72edc7853eac176
Dmitry862 <rk3tt@mail.ru>
2024-11-08 00:34:27 +0300

    feat: orderAlphabetically

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -303,8 +303,8 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
