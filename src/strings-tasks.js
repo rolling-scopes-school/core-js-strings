dca3726ad4e9bcbb527db64d950b8b46450619de
Dmitry862 <rk3tt@mail.ru>
2024-11-11 21:11:08 +0300

    feat: formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -262,8 +262,9 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  // throw new Error('Not implemented');
+  return `${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2, 0)}`;
 }
 
 /**
