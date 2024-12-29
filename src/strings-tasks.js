0b2fb6a56d6e2fe285a4c07f8dd767ca815193aa
ann360 <82609121+ann360@users.noreply.github.com>
2024-12-29 13:41:01 +0400

    improved godpleasework

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,11 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  if (typeof value !== 'string') {
+    return 0;
+  }
+  return value.length; ;
 }
 
 /**
